b6bfaaaf3a4330a708ddeb7ad32f27d5ec1db231
vardhan1564 <vardhanadheli1527@gmail.com>
2026-04-24 22:56:01 +0530

    feat: added JWT dependencies, Auth DTOs, and JwtService token generator

---
+++ backend/src/main/java/com/ats/backend/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.ats.backend.dto;
+
+import com.ats.backend.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private Role role;
+}
